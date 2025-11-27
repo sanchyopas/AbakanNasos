35190d28d94c454ed61fa9889e82db6ce39f2863
avroratomsk <avroratomsk70@yandex.ru>
2025-11-27 11:57:17 +0300

    add debug false

--- main/main/settings.py
+++ main/main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-$a=0=omhq&u5)_!a62w!susqi&&vnb0op@lyh$f_rjp*wvsnp&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
