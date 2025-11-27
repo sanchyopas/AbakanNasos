0300e6120a8fb91addf8aab2e480a0edba540403
avroratomsk <avroratomsk70@yandex.ru>
2025-11-27 14:33:51 +0300

    аÑfix

--- main/main/urls.py
+++ main/main/urls.py
@@ -34,8 +34,8 @@ if settings.DEBUG:
     pass
   except ImportError:
     import debug_toolbar
-      urlpatterns += [
-        path("__debug__/", include(debug_toolbar.urls)),
-      ]
+    urlpatterns += [
+      path("__debug__/", include(debug_toolbar.urls)),
+    ]
     
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
