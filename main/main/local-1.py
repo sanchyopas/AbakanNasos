8c67b079a2d092f7e955a4efeec0943fd5b9a0ba
avroratomsk <avroratomsk70@yandex.ru>
2025-11-27 11:50:13 +0300

    add middleware

--- main/main/local-1.py
+++ main/main/local-1.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     # "debug_toolbar.middleware.DebugToolbarMiddleware",
     # "corsheaders.middleware.CorsMiddleware",
 ]
