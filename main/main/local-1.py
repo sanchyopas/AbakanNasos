b1cc074bff80457ef5c72567ad5a0a41bf6d717c
avroratomsk <avroratomsk70@yandex.ru>
2025-11-27 12:56:28 +0300

    add debug false

--- main/main/local-1.py
+++ main/main/local-1.py
@@ -39,7 +39,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
     # "debug_toolbar.middleware.DebugToolbarMiddleware",
     # "corsheaders.middleware.CorsMiddleware",
 ]
