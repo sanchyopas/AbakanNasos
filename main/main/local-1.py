295c130711982b872cd71a1b79fbedce9d8b48c0
avroratomsk <avroratomsk70@yandex.ru>
2025-11-27 11:12:10 +0300

    аÑfix

--- main/main/local-1.py
+++ main/main/local-1.py
@@ -41,6 +41,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     # "debug_toolbar.middleware.DebugToolbarMiddleware",
     # "corsheaders.middleware.CorsMiddleware",
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 DATABASES = {
