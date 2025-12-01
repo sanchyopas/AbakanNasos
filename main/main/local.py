f749150f84f164352bbf4c67a3f1d367b0d92a06
avroratomsk <avroratomsk70@yandex.ru>
2025-12-01 12:18:40 +0300

    dev

--- main/main/local.py
+++ main/main/local.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     # "coupons",
     "users",
     "service",
-#     'django.contrib.sites',
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
