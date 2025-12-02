451e68297b9a8ebd6bfcca0832ac7318c889629f
avroratomsk <avroratomsk70@yandex.ru>
2025-12-02 11:47:33 +0300

    Ðfix

--- main/blog/urls.py
+++ main/blog/urls.py
@@ -4,7 +4,6 @@ from blog import views
 
 urlpatterns = [
     path('', views.blog, name="blog"),
-
     path('<slug:category_slug>/', views.category_post, name='category_post'),
     path('post/<slug:slug>/', views.post, name='post_without_category'),
     path('<slug:category_slug>/<slug:slug>/', views.post, name="post"),
